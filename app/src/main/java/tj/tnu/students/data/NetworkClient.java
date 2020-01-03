528fc65ceb5054ac0697830c22c7448eca163526
Khusrav Ashurzoda <khusrav2000@mail.ru>
2020-01-03 10:52:56 +0500

    Версия 1.0.0

--- app/src/main/java/tj/tnu/students/data/NetworkClient.java
+++ app/src/main/java/tj/tnu/students/data/NetworkClient.java
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 // Соединения с сервером.
 public class NetworkClient {
 
-    public static final String BASE_URL = "http://192.168.0.106:5000";
+    public static final String BASE_URL = "http://77.95.1.55:7575";
 
     public static Retrofit retrofit;
 
